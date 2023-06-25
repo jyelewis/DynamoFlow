217eb66edfd410d8d7d307b61b2ade1b08b22bdf
Jye Lewis <jye@jyelewis.com>
2023-06-25 14:56:28 +1000

    Provide mock AWS creds for DDB Local test environment

--- jest.globalSetup.ts
+++ jest.globalSetup.ts
@@ -5,6 +5,10 @@ import {
 import { DFDB } from "./src/DFDB.js";
 
 async function jestGlobalSetup() {
+  // use mock AWS keys (required present for DDB Local)
+  process.env.AWS_ACCESS_KEY_ID = "XXXXXXXXXXXXX";
+  process.env.AWS_SECRET_ACCESS_KEY = "XXXXXXXXXXXXX";
+
   // create required test tables
   await Promise.all([
     new DFDB(testDbConfig).createTableIfNotExists(),
